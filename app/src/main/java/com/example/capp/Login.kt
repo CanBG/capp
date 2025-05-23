5fd7b21d6a8f8796993aaec7bccb0d40fc9cfdb4
CanBG <cansarper1@hotmail.com>
2025-05-23 17:54:40 +0300

    commit

---
+++ app/src/main/java/com/example/capp/Login.kt
@@ -0,0 +1,78 @@
+package com.example.capp
+
+import android.app.ActionBar
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.capp.ui.theme.CappTheme
+import com.google.firebase.auth.FirebaseAuth
+
+class Login : AppCompatActivity() {
+
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignup: Button
+
+    private lateinit var mAuth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+        supportActionBar?.hide()
+
+        mAuth = FirebaseAuth.getInstance()
+
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogin = findViewById(R.id.btn_login)
+        btnSignup = findViewById(R.id.btn_signup)
+
+        btnSignup.setOnClickListener{
+            val intent = Intent(this, Signup::class.java)
+            startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener{
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email,password);
+        }
+
+
+
+
+
+    }
+
+    private fun login(email: String, password: String){
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@Login, MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@Login, "User does not exist", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    }
+}
---
+++ app/src/main/java/com/example/capp/Signup.kt
@@ -0,0 +1,80 @@
+package com.example.capp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.capp.ui.theme.CappTheme
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class Signup : AppCompatActivity() {
+
+    private lateinit var edtName: EditText
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnSignup: Button
+
+    private lateinit var mAuth: FirebaseAuth
+
+    private lateinit var mDbRef: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        supportActionBar?.hide()
+
+        mAuth = FirebaseAuth.getInstance()
+
+        edtEmail = findViewById(R.id.edt_email)
+        edtName = findViewById(R.id.edt_name)
+        edtPassword = findViewById(R.id.edt_password)
+        btnSignup = findViewById(R.id.btn_signup)
+
+        btnSignup.setOnClickListener{
+            val name = edtName.text.toString()
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            signup(name,email,password)
+        }
+
+    }
+
+    private fun signup(name:String, email: String, password: String){
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    addUserToDatabase(name,email,mAuth.currentUser?.uid!!)
+                    val intent = Intent(this@Signup, MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@Signup, "Something went wrong", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    }
+
+    private fun addUserToDatabase(name: String, email: String, uid: String){
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+
+        mDbRef.child("user").child(uid).setValue(User(name,email,uid))
+
+    }
+}
